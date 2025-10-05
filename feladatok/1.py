ccef2ba0949766f23e1249ce51c8e049f3a5d36b
Roli0410 <muller.gabor.roland@diak.szbi-pg.hu>
2025-10-05 17:28:08 +0200

    v1

---
+++ feladatok/1.py
@@ -0,0 +1,13 @@
+"""1. Összegszámítás
+Kérj be egy egész számot (pl. 10; 13;  20…), és számítsd ki az 1-től a megadott számig terjedő egész számok összegét.
+"""
+
+szam = int(input("Adj meg egy egész számot!"))
+
+osszeg = 0
+
+for i in range(1,  szam + 1)
+        osszeg += i
+
+
+print("Az összeg:", osszeg)
